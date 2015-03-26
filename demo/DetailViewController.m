fb2739e2dfffd612b5bccff2f5b3e15560886a60
leechannl <leechannl@gmail.com>
2015-03-26 12:23:26 +0800

    Draw a menu button.

--- demo/DetailViewController.m
+++ demo/DetailViewController.m
@@ -10,30 +10,69 @@
 
 @interface DetailViewController ()
 
+@property (nonatomic, strong) UIImage *menuButton;
+
 @end
 
 @implementation DetailViewController
+
+- (UIImage *)menuButton {
+
+
+    if (_menuButton != nil) {
+        return _menuButton;
+    }
+
+    // Setup some sizing variables
+    CGFloat spacing     = 4.0f;  // Spacing between each menu button strip
+    CGFloat stripHeight = 1.5f;  // Height of each menu button strip, total of 3
+    CGFloat width       = 24.0f; // Total button width, also width of each strip
+    CGFloat height      = spacing * 2 + stripHeight * 3;
+
+    // Setup the context rect
+    CGRect rect = CGRectMake(0.0f, 0.0f, width, height);
+
+    // Passing 0.0f will ensure that the context uses the screen scale (3.0 for iPhone 6 Plus)
+    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0.0f);
+
+    // Fill the button with black
+    [[UIColor blackColor] set];
+
+    // Some simple math to render the menu button lines
+    for (int i=0;i<3;i++) {
+        UIBezierPath *path = [UIBezierPath bezierPathWithRect:CGRectMake(0.0f, i * (stripHeight + spacing), rect.size.width, stripHeight)];
+        [path fill];
+    }
+
+    // Assign complete image while adjusting it to use UIImageRenderingModeAlwaysTemplate
+    // UIImageRenderingModeAlwaysTemplate will ensure the image is tinted with the current tint color
+
+    _menuButton = [UIGraphicsGetImageFromCurrentImageContext() imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    UIGraphicsEndImageContext();
+
+    return _menuButton;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
     self.navigationController.interactivePopGestureRecognizer.enabled = NO;
+
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[self menuButton]
+                                                                             style:UIBarButtonItemStyleDone
+                                                                            target:self
+                                                                            action:@selector(menuPressed)];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)menuPressed
+{
+
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidDisappear:(BOOL)animated
+{
+    self.navigationController.interactivePopGestureRecognizer.enabled = YES;
 }
-*/
 
 @end
